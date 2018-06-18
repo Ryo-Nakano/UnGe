9f34b1ae5585ff4f371c2a2bb5f81b8069dea03a
Ryohei Nanano <matsuhara.9999ryo@gmail.com>
2018-06-19 02:49:16 +0900

    ▶︎オンラインランキング実装完了！ ・HigiScoreの値のみによって順位づけ ・データはダミー

--- Assets/Scripts/Ranking/RankingManager.cs
+++ Assets/Scripts/Ranking/RankingManager.cs
@@ -1,13 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;//シーン遷移扱う
+using UnityEngine.UI;
+using NCMB;//NCMB使う！
 
 public class RankingManager : MonoBehaviour {
+    
+    //HighScoreランキング表示の為のラベル
+	[SerializeField] Text[] higehScoreText = new Text[5];
+	[SerializeField] Text[] userText = new Text[5];
+	[SerializeField] Text[] aveText = new Text[5];
 
 	// Use this for initialization
 	void Start () {
-		
+		fetchTopRankers();
 	}
 	
 	// Update is called once per frame
@@ -15,8 +21,29 @@ public class RankingManager : MonoBehaviour {
 		
 	}
 
-	//MainMenueに戻るメソッド定義
-	public void BackToMainMenue(){//ボタンから呼び出すからpublic
-		SceneManager.LoadScene("MainMenue");
-	}
+	public void fetchTopRankers()
+    {
+        // データストアの「HighScore」クラスから検索
+		NCMBQuery<NCMBObject> query = new NCMBQuery<NCMBObject>("OnlineRanking");
+        query.OrderByDescending("HighScore");
+        query.Limit = 5;
+        query.FindAsync((List<NCMBObject> objList, NCMBException e) => {
+
+            if (e != null)//エラーあった時
+            {
+                //検索失敗時の処理
+            }
+            else//うまく行ったとき
+            {
+				for (int i = 0; i < objList.Count; i++)//objListの要素数と同じだけfor回す
+				{
+					//順次ランキング表示！
+					userText[i].text = System.Convert.ToString(objList[i]["UserName"]);
+					higehScoreText[i].text = System.Convert.ToString(objList[i]["HighScore"]);
+					aveText[i].text = System.Convert.ToString(objList[i]["Ave"]);
+				}
+            }
+        });
+    }
+
 }
