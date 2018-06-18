781d7f089639d9b531a23409df1606296cdc131a
Ryohei Nanano <matsuhara.9999ryo@gmail.com>
2018-06-19 00:54:43 +0900

    ▶︎Rankingの画面遷移実装完了 (遷移先の中身は手付かず。遷移のアニメーションのみ)

--- Assets/Scripts/MainMenue/MainMenueManager.cs
+++ Assets/Scripts/MainMenue/MainMenueManager.cs
@@ -9,20 +9,19 @@ public class MainMenueManager : MonoBehaviour {
 	AudioSource audioSource;//取得したコンポーネント格納しておく為の変数定義！
 	[SerializeField] GameObject bgm;//Unityからアタッチ
 
+    //=====以下、画面遷移の為の変数=====
+	[SerializeField] GameObject view2;//View2をUnityからアタッチ
+    Animator animator;//取得したAnimatorを格納しておく為の変数
+
 	// Use this for initialization
 	void Start () {
 		audioSource = gameObject.GetComponent<AudioSource> ();//AudioSourceのコンポーネント取得、AudioSourceクラスのaudioSource変数に格納！
+		animator = view2.gameObject.GetComponent<Animator>();//View2についてるAnimatorを取得→変数animatorに格納
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
-	}
-
-	//Rankingに移動するメソッド定義
-	public void MoveToRanking(){//ボタンに割り当ての為public
-//		audioSource.Stop(titleMusic);
-		StartCoroutine("MoveRankingWithTimer");
 	}
 
 	//GamePlayingに移動するメソッド定義
@@ -44,4 +43,21 @@ public class MainMenueManager : MonoBehaviour {
 		Destroy(bgm);
 		SceneManager.LoadScene ("GamePlaying");//シーン移動します
 	}
+
+
+    //=====以下、画面遷移の為の関数=====
+
+	//View2に遷移する為の関数
+    public void GoToRankingView()
+    {
+        animator.SetBool("running", true);
+        Debug.Log("GoToView2");
+    }
+
+    //View1に戻る為の関数
+    public void BackToMainMenu()
+    {
+        animator.SetBool("running", false);
+        Debug.Log("BackToView1");
+    }
 }
