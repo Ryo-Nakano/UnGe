31ed42e7fd78350546a39bcf71a2023e2ebddf0f
Ryohei Nanano <matsuhara.9999ryo@gmail.com>
2018-06-20 17:19:01 +0900

    ▶︎小ネタぶち込み

--- Assets/Scripts/MainMenue/Profile/ProfileManager.cs
+++ Assets/Scripts/MainMenue/Profile/ProfileManager.cs
@@ -30,36 +30,68 @@ public class ProfileManager : MonoBehaviour {
 		{
 			if(inputField.text.Length <= 6)//文字数6文字以下の時
 			{
-                //InputFieldへの入力内容をNCMBに保存
-                NCMBQuery<NCMBObject> query = new NCMBQuery<NCMBObject>("OnlineRanking");
-                query.WhereEqualTo("objectId", PlayerPrefs.GetString("objectId"));
-                query.FindAsync((List<NCMBObject> objList, NCMBException e) => {
-                    if (e != null)
-                    {
-                        //検索失敗時の処理
-                        Debug.Log("ミスっとるで！");
-                    }
-                    else
-                    {
-                        //値の更新
-						objList[0]["UserName"] = inputField.text;//プレイ総数
-						Debug.Log("inputField2 : " + inputField.text);
-                        objList[0].SaveAsync();//変更内容のsave
-						inputField.text = "";//空白に直す
-                    }
-                });
+				if(inputField.text == "うんこ" ||
+				   inputField.text == "ちんこ" ||
+				   inputField.text == "まんこ" ||
+				   inputField.text == "おっぱい" ||
+				   inputField.text == "死ね" )
+				{
+					alertText.text = "『秩序が乱れる。やめい。』";
+				}
+				else if (inputField.text == "セリヌン")
+                {
+                    alertText.text = "『セリヌン...？ お前まさか...！！』";
+                }
+				else if (inputField.text == "青野" ||
+				         inputField.text == "りょーすけ")
+                {
+                    alertText.text = "『それは顔面。』";
+                }
+				else//秩序を乱さない入力内容だった時
+				{
+					//InputFieldへの入力内容をNCMBに保存
+                    NCMBQuery<NCMBObject> query = new NCMBQuery<NCMBObject>("OnlineRanking");
+                    query.WhereEqualTo("objectId", PlayerPrefs.GetString("objectId"));
+                    query.FindAsync((List<NCMBObject> objList, NCMBException e) => {
+                        if (e != null)
+                        {
+                            //検索失敗時の処理
+                            Debug.Log("検索ミス！");
+                        }
+                        else
+                        {
+                            //値の更新
+                            objList[0]["UserName"] = inputField.text;//プレイ総数
+                            Debug.Log("inputField2 : " + inputField.text);
+                            objList[0].SaveAsync();//変更内容のsave
+                            inputField.text = "";//空白に直す
+                        }
+                    });
 
-				mainMenueManager.BackToMainMenu2();//View3→View1に画面遷移
-				alertText.text = "";//Alertを空白に戻す
+                    mainMenueManager.BackToMainMenu2();//View3→View1に画面遷移
+                    alertText.text = "";//Alertを空白に戻す
+				}
 			}
 			else//文字数6文字より多い時
 			{
-				alertText.text = "※文字数が多すぎるでごわす！";
+				if(inputField.text == "セリヌンティウス" ||
+				   inputField.text == "せりぬんてぃうす")//セリヌンティウスの時
+				{
+					alertText.text = "『お前まさか...！！』";
+				}
+				else if(inputField.text == "パスタ作ったお前")
+				{
+					alertText.text = "『まさか...湘南乃風...！？！？』";
+				}
+				else
+				{
+					alertText.text = "『文字数が多すぎるでごわす！』";
+				}
 			}
 		}
 		else//文字入力してない時
 		{
-			alertText.text = "※文字が入力されていないでごわす！";
+			alertText.text = "『文字が入力されていないでごわす！』";
 		}
 	}
 }
